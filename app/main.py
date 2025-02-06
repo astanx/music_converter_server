d606502e72fc5ff954b702ad656f46f212aacdce
Ivan <astanin.i.a@gmail.com>
2025-02-06 18:03:28 +0300

    added run to main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import uvicorn
 from app.database.connection import database
 from app.middlewares.cors import add_cors
 from app.api import users, music
@@ -14,6 +16,9 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-
 app.include_router(users.router, prefix="/users")
 app.include_router(music.router, prefix="/music")
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
